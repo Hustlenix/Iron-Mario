85b0c7490436941ab35b622e9b047d0003d7f019
Window Genie <info@windowgenie.local>
2026-08-06 18:28:45 +0530

    Make settings scene real

---
+++ scenes/settings_scene.gd
@@ -0,0 +1,30 @@
+extends Control
+
+@onready var volume_slider: HSlider = $CenterContainer/VBoxContainer/VolumeRow/VolumeSlider
+@onready var status_label: RichTextLabel = $CenterContainer/VBoxContainer/StatusLabel
+
+func _ready() -> void:
+	volume_slider.value = Global.volume
+	_apply_volume(Global.volume)
+
+func _apply_volume(value: float) -> void:
+	AudioServer.set_bus_volume_db(0, linear_to_db(maxf(value, 0.001) / 100.0))
+
+func _on_volume_changed(value: float) -> void:
+	Global.volume = value
+	_apply_volume(value)
+
+func _on_reset_button_pressed() -> void:
+	Global.best_streak = 0
+	Global.streak = 0
+	Global.loop = 0
+	Global.save()
+	status_label.text = "PROGRESS RESET (SAVED)"
+	status_label.visible = true
+
+func _on_back_button_pressed() -> void:
+	Global.save()
+	get_tree().change_scene_to_file("res://scenes/title_screen.tscn")
+
+func _exit_tree() -> void:
+	Global.save()
--- scenes/title_screen.gd
+++ scenes/title_screen.gd
@@ -1,13 +1,14 @@
 extends Node2D
 
+func _ready() -> void:
+	$Bgm.stream.loop_mode = AudioStreamWAV.LOOP_FORWARD
+	$Bgm.play()
+
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/level_scene.tscn")
 
 func _on_settings_button_pressed() -> void:
-	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
-	else:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	get_tree().change_scene_to_file("res://scenes/settings_scene.tscn")
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
