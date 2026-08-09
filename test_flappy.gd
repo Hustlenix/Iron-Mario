7129ce2918cb43a261655f388db2f28132b36d70
Window Genie <info@windowgenie.local>
2026-08-09 06:43:22 +0530

    feat(flappy): core loop, pipe pool, scoring, collisions

--- test_flappy.gd
+++ test_flappy.gd
@@ -6,12 +6,27 @@ func _initialize() -> void:
 	var scene := load("res://scenes/flappy_bird.tscn") as PackedScene
 	flappy = scene.instantiate()
 	root.add_child(flappy)
+	var args := OS.get_cmdline_user_args()
+	if args.has("--flap"):
+		await _test_flap()
+	else:
+		_run_frames(30)
+		if flappy.get_state() == "title":
+			print("FLAPPY BOOT OK")
+			quit(0)
+		else:
+			print("FLAPPY BOOT FAIL: state=%s" % flappy.get_state())
+			quit(1)
+
+func _test_flap() -> void:
+	flappy.flap()
+	await process_frame
 	_run_frames(30)
-	if flappy.get_state() == "title":
-		print("FLAPPY BOOT OK")
+	if flappy.get_state() == "playing":
+		print("FLAP TEST OK")
 		quit(0)
 	else:
-		print("FLAPPY BOOT FAIL: state=%s" % flappy.get_state())
+		print("FLAP TEST FAIL: state=%s" % flappy.get_state())
 		quit(1)
 
 func _run_frames(count: int) -> void:
